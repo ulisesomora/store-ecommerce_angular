e2584440bcce50f8d96523088d1f6beb36723357
Ulises Mora <ulisote.omar@gmail.com>
2024-08-13 08:11:41 -0600

    check the product interface

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -12,21 +13,13 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
 
   hideSideMenu = signal(true);
-  @Input({required:true}) cart:Product[] =[];
-  total =signal(0)
+  
+  private cartService = inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.total
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState)
   }
 
-  ngOnChange(changes: SimpleChanges){
-    const cart = changes['cart']
-    if(cart){
-      this.total.set(this.calcTotal())
-    }
-  }
-
-  calcTotal(){
-    return this.cart.reduce((total, product) => total + product.price, 0)
-  }
 }
