589960f96f8193a05c0d8f0710be9c2bbbd0df95
Ulises Mora <ulisote.omar@gmail.com>
2024-08-13 08:24:11 -0600

    Add API in the project

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../shared/models/product.model';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -16,54 +17,20 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
   private cartService = inject(CartService)
+  private productService = inject(ProductService);
 
-  constructor(){
-    const initProducts: Product[] =[
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString()
-      }, 
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=76',
-        creationAt: new Date().toISOString()
-      }, 
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=4503',
-        creationAt: new Date().toISOString()
-      }, 
-      {
-        id: Date.now(),
-        title: 'Pro 4',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=323',
-        creationAt: new Date().toISOString()
-      }, 
-      {
-        id: Date.now(),
-        title: 'Pro 5',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=435',
-        creationAt: new Date().toISOString()
-      }, 
-      {
-        id: Date.now(),
-        title: 'Pro 6',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=676',
-        creationAt: new Date().toISOString()
-      }, 
-    ];
-    this.products.set(initProducts);
+  ngOnInit(){
+    this.productService.getProducts()
+      .subscribe({
+        next: (products)=>{
+          this.products.set(products)
+        },
+        error: ()=>{
+          
+        }
+      })
   }
+  
   addToCart( product: Product){
     this.cartService.addToCart(product)
   }
